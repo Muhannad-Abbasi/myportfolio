99d6d824c603d563fcbfd21bbcd3b2c57452bcf6
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2025-04-08 20:19:13 +0200

    final touches

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,8 +1,7 @@
 import about from './about'
 import contact from './contact'
 import skills from './skills'
-import work from './work'
+import works from './works'
 import testimonials from './testimonials'
-// import experiences from './experiences'
 
-export const schemaTypes = [about, contact, skills, work, testimonials]
+export const schemaTypes = [about, contact, skills, works, testimonials]
--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -40,7 +40,7 @@ const Work = () => {
     <>
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-      <div className="app__work-filter">
+      {/* <div className="app__work-filter">
         {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
@@ -50,26 +50,26 @@ const Work = () => {
             {item}
           </div>
         ))}
-      </div>
+      </div> */}
 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        {filterWork.map((work, index) => (
+        {filterWork?.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div
               className="app__work-img app__flex"
             >
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img src={urlFor(work?.imgUrl)} alt={work?.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work?.projectLink} target="_blank" rel="noreferrer">
 
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
@@ -80,7 +80,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={work?.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
@@ -94,12 +94,7 @@ const Work = () => {
             </div>
 
             <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
-
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
-              </div>
+              <h4 className="bold-text">{work?.title}</h4>
             </div>
           </div>
         ))}
